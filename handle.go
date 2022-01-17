9e64d3a6fe1344c2df0d9fe92ce3ca2784955256
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-01-17 21:03:42 +0100

    Implement main handler

---
+++ handle.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+type paramkey string
+
+const (
+	paramkeyDelay paramkey = "delay"
+	paramkeyFib   paramkey = "fib"
+)
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+
+	delayMS, err := readParamInt(params, paramkeyDelay)
+	if err != nil {
+		respondError(w, 400, err)
+		return
+	}
+
+	fibIndex, err := readParamInt(params, paramkeyFib)
+	if err != nil {
+		respondError(w, 400, err)
+		return
+	}
+
+	if delayMS > 0 {
+		time.Sleep(time.Duration(delayMS) * time.Millisecond)
+	}
+
+	if fibIndex > 0 {
+		fibonacci(fibIndex) //nolint
+	}
+}
+
+func readParamInt(params url.Values, key paramkey) (int, error) {
+	raw := params.Get(string(key))
+	if raw == "" {
+		return 0, nil
+	}
+
+	n, err := strconv.Atoi(raw)
+	if err != nil {
+		return 0, fmt.Errorf("invalid param: %s: want int, got %s", key, raw)
+	}
+
+	return n, nil
+}
+
+func respondError(w http.ResponseWriter, code int, err error) {
+	w.WriteHeader(code)
+	w.Write([]byte(err.Error()))
+}
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const defaultPort = "9999"
+
+var port = flag.String("port", defaultPort, "listening port")
 
 func main() {
-	fmt.Println("Hi!")
+	flag.Parse()
+
+	addr := ":" + *port
+	handler := http.HandlerFunc(handle)
+
+	fmt.Printf("http://localhost:%s\n", *port)
+	log.Fatal(http.ListenAndServe(addr, handler))
 }
