4837d13e108f1f967745e8ec4be93ea3ebef857e
moreirathomas <moreirathomas97@gmail.com>
2022-03-11 00:58:32 +0100

    feat: mock each benchttp data endpoint

--- server/mocks.go
+++ server/mocks.go
@@ -10,5 +10,34 @@ func mockFile(file string) string {
 }
 
 func (s *Server) handleMocks(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, mockFile("report-get.json"))
+	if r.Method != "GET" {
+		http.NotFound(w, r)
+		return
+	}
+
+	var root string
+	root, r.URL.Path = shiftPath(r.URL.Path)
+
+	switch root {
+	case "reports":
+		if r.URL.Path == "/" {
+			// There is no GET /reports
+			http.NotFound(w, r)
+		} else {
+			// Serve mock of /reports/{id}
+			http.ServeFile(w, r, mockFile("report.json"))
+		}
+
+	case "stats":
+		if r.URL.Path == "/" {
+			// Serve mock of /stats
+			http.ServeFile(w, r, mockFile("stats-list.json"))
+		} else {
+			// Serve mock of /stats/{id}
+			http.ServeFile(w, r, mockFile("stats.json"))
+		}
+
+	default:
+		http.NotFound(w, r)
+	}
 }
--- server/server.go
+++ server/server.go
@@ -3,6 +3,8 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"path"
+	"strings"
 )
 
 type Server struct {
@@ -11,25 +13,20 @@ type Server struct {
 	requestCount int64
 }
 
-type urlpath string
-
-const (
-	urlpathDebug    urlpath = "/debug"
-	urlpathIdentity urlpath = "/identity"
-	urlpathMocks    urlpath = "/report"
-)
-
 func New(port string) *Server {
 	return &Server{port: port}
 }
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch urlpath(r.URL.Path) {
-	case urlpathDebug:
+	var root string
+	root, r.URL.Path = shiftPath(r.URL.Path)
+
+	switch root {
+	case "debug":
 		s.handleDebug(w, r)
-	case urlpathIdentity:
+	case "identity":
 		s.handleIdentity(w, r)
-	case urlpathMocks:
+	case "mocks":
 		s.handleMocks(w, r)
 	default:
 		s.handleFibonacci(w, r)
@@ -41,4 +38,17 @@ func (s *Server) ListenAndServe() error {
 	fmt.Printf("http://%s\n", addr)
 
 	return http.ListenAndServe(addr, s)
+}
+
+// shiftPath splits off the first fragment of p.
+// head will never contain a slash and tail will
+// always be a rooted path without trailing slash.
+// 	/foo/bar?x=1234 -> foo, /bar?x=1234
+func shiftPath(p string) (head, tail string) {
+	p = path.Clean("/" + p)
+	i := strings.Index(p[1:], "/") + 1
+	if i <= 0 {
+		return p[1:], "/"
+	}
+	return p[1:i], p[i:]
 }
